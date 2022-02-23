4c3f8ff2ba8ded9764a304017db5e249a3ccc311
mepino <76593972+mepino@users.noreply.github.com>
2022-02-23 18:13:50 +0900

    Time: 1245 ms (63.78%), Space: 25 MB (62.11%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,13 @@
-import numpy as np
-
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self,prices):
+        left = 0 #Buy
+        right = 1 #Sell
         max_profit = 0
-        historical_min_val = np.inf
-
-        for i in range(len(prices)-1):
-            if (i == 0) or (prices[i] < historical_min_val):
-                buy = prices[i]
-                historical_min_val = prices[i]
-                
-                sell = max(prices[i+1:])
-                profit = sell - buy
-                if profit > max_profit:
-                    max_profit = profit
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
             else:
-                continue
+                left = right
+            right += 1
         return max_profit
