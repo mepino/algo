82600e88343cf28be0b0c7e85a036583d4f972eb
mepino <76593972+mepino@users.noreply.github.com>
2022-02-24 10:57:32 +0900

    Time: 728 ms (46.22%), Space: 15.2 MB (88.80%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,40 @@
+class Solution:
+    # def canJump(self, nums: List[int]) -> bool:
+    #     nums.reverse()
+    #     while len(nums) > 1:
+    #         i_list = [0 if val-i >= 0 else -1 for i, val in enumerate(nums)]
+    #         try:
+    #           i_list.index(0)
+    #           i_list.reverse()
+    #           new_start = len(i_list) - i_list.index(0) - 1
+    #           if new_start == 0:
+    #             return False
+    #           nums = nums[new_start:]
+    #         except:
+    #           return False
+    #     return True
+#         nums.reverse()
+#         while len(nums) > 1:
+#           i_list = []
+
+#           for i, val in enumerate(nums):
+#             if i == 0:
+#               continue
+#             if val-i >= 0:
+#               i_list.append(i)
+#           if len(i_list) == 0:
+#             return False
+#           new_start = max(i_list)
+#           new_nums = nums[new_start:]
+#           if new_nums == nums:
+#             return False
+#           nums = new_nums
+#         return True      
+
+    def canJump(self, nums):
+        m = 0
+        for i, n in enumerate(nums):
+            if i > m:
+                return False
+            m = max(m, i+n)
+        return True
